85185be98c8358bd95e68371ef9aec31975bb691
ilvro <cildumyt@gmail.com>
2023-03-05 01:06:29 -0300

    interactables WIP
    interactables are being worked on, dialogue should also change a bit

--- LAURA/Assets/Interactable.cs
+++ LAURA/Assets/Interactable.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Interactable : MonoBehaviour
 {
-    public bool inRange;
-    public KeyCode InteractKey;
+    public bool isInRange;
+    public KeyCode interactKey;
+    public TMP_Text interactText;
+    public Transform characterTransform;
     void Start()
     {
-        
+        interactText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInRange)
+        {
+            interactText.rectTransform.position = Camera.main.WorldToScreenPoint(characterTransform.position) + new Vector3(0, 80, 0);
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction();
+            }
+        }
+    }
+
+    void interactAction()
+    {
+        Debug.Log("interacted!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // player can interact with the object
+            isInRange = true;
+
+            // make the gui visible
+            interactText.text = $"Interact [{interactKey}]";
+            interactText.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // player exited interactable area
+            isInRange = false;
+
+            // hide the gui
+            interactText.gameObject.SetActive(false);
+        }
     }
 }
