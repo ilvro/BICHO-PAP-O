0a7bc812dda62efc6878ce8a1ceef24a9820c3b9
ilvro <cildumyt@gmail.com>
2023-03-14 16:46:39 -0300

    changed volume component
    vignette works now woooo (sort of)

--- LAURA/Assets/Scripts/VignetteController.cs
+++ LAURA/Assets/Scripts/VignetteController.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class VignetteController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float intensity = 0.5f;
+    public float smoothness = 0.5f;
+
+    private Volume volume;
+    private Vignette vignette;
+
     void Start()
     {
-        
+        volume = gameObject.AddComponent<Volume>();
+        volume.sharedProfile = new VolumeProfile();
+        vignette = volume.sharedProfile.Add<Vignette>();
+        vignette.intensity.Override(intensity);
+        vignette.smoothness.Override(smoothness);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        vignette.intensity.Override(intensity);
+        vignette.smoothness.Override(smoothness);
+    }
+
+    public void SetIntensity(float newIntensity)
+    {
+        intensity = newIntensity;
+    }
+
+    public void SetSmoothness(float newSmoothness)
+    {
+        smoothness = newSmoothness;
     }
 }
